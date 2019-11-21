fdbeee2bb1c2413ce75970af5dcef31e5ab9065c
Ivan_Vatlin <ivan_valtin@epam.com>
2019-11-21 18:19:20 +0400

    Initial configuration added

--- business-services/employees-api/src/main/groovy/com/epam/hw/netflix/EmployeesApplication.groovy
+++ business-services/employees-api/src/main/groovy/com/epam/hw/netflix/EmployeesApplication.groovy
@@ -2,8 +2,10 @@ package com.epam.hw.netflix
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 
 @SpringBootApplication
+@EnableDiscoveryClient
 class EmployeesApplication {
     static void main(String[] args) {
         SpringApplication.run(EmployeesApplication, args)
