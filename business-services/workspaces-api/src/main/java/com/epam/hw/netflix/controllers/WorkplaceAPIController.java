f8efcb64ad3dc0f44ca88737b6b6586db4d62ae7
Ivan_Vatlin <ivan_valtin@epam.com>
2019-11-22 17:20:42 +0400

    Initial configuration added

--- business-services/workspaces-api/src/main/java/com/epam/hw/netflix/controllers/WorkplaceAPIController.java
+++ business-services/workspaces-api/src/main/java/com/epam/hw/netflix/controllers/WorkplaceAPIController.java
@@ -5,9 +5,7 @@ import com.epam.hw.netflix.domain.Workspace;
 import com.epam.hw.netflix.services.WorkplaceService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
